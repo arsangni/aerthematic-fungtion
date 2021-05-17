9e853273c8e4ba460570fc558a971778315c2004
arsangni <arsangni@gmail.com>
2021-05-17 12:32:10 +0530

    first commit

---
+++ arithmatic functions.R
@@ -0,0 +1,69 @@
+num69 = 56
+num420 = 465
+num69 + num420
+num420 - num69
+num420 * num69
+num420 / num69
+num420< num69
+num420 > num69
+num10=num69
+num10
+a=1
+b=a
+1+2
+sqrt(64) for square recordPlot
+#exponents
+10^3
+2^2
+34*69
+2000/20
+?sqrt
+?mean
+sqrt(87)
+mean(1,10,100)
+mean(c(1,10,100))
+a<- c(1,10,100)
+mean (a)
+sum(a)
+max(a)
+min(a)
+a=1
+a
+str(a)
+class(a)
+b=20
+str(b)
+class(b)
+b= "dog"
+b
+str(b)
+class(b)
+a= "x1"
+a
+class(a)
+str(a)
+a<- 1:10
+a
+a<- 1:5
+a
+str(a)
+class(a)
+f<- c(69.69,68,87,74.78,1:4)
+f
+f[c(2,3)]
+as.integer (f)
+round(f)
+j<-c("1.8","2.4","5.5")
+sum(j)
+sum(as.numeric(j))
+round(as.numeric(j))
+is.numeric(j)
+is.character(j)
+n <- as.numeric(j)
+is.numeric(n)
+numeric()
+b <- c(1,1.9,3.0,69.69,4:9)
+str(b)
+class(b)
+b[c(3,4)]
+
